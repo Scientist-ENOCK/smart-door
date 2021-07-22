3f30859204c9cbea05176eaed37406bafbd0bcf0
Corama Yunus <coramayunus@gmail.com>
2021-07-22 22:18:02 +0300

    minor update and layout improvements

--- javascript/manage.js
+++ javascript/manage.js
@@ -9,7 +9,6 @@ const displayMessage = (message, errorPlace) => {
 }
 
 window.onload = () => {
-    console.log('hello');
     getTenants();
 }
 fetchTenants.addEventListener('click', () => {
@@ -25,7 +24,6 @@ const getTenants = async () => {
         }
     });  
     const response = await rawFetch.json();
-    console.log(response.tenants);
     if(response.success){
       response.tenants.forEach(tenant => {
         // const row = document.createElement('tr');
@@ -38,15 +36,29 @@ const getTenants = async () => {
           newRow.insertCell(3).appendChild(document.createTextNode(tenant.duration))
           newRow.insertCell(4).appendChild(document.createTextNode(tenant.endDuration))
           newRow.insertCell(5).appendChild(document.createTextNode(tenant.status))
+  
+          // newRow.insertCell(6).appendChild(document.createElement('button').type = 'button'.style.background = 'red');
         //   newRow.appendChild(tdName);
         //   document.querySelector("#looped-tenants").appendChild(row);
       });
-        return;
+        // return;
     }
-    console.log(response);
+    // console.log(response);
     displayMessage(response.message, 'error');  
   } catch (error) {
-    displayMessage("Oops, something went wrong", 'error-login');
+    displayMessage("Oops, something went wrong", 'error');
       console.log(error);
   }
 }
+
+// const deleteControl = (tenantId)=> {
+//   const form = document.createElement('form');
+//   const input = document.createElement('input');
+//   input.type = 'hidden';
+//   input.value = tenantId;
+//   const deleteBtn = document.createElement('button').type = 'button'.style.background = 'red';
+//   deleteBtn.addEventListener('click', ()=>{
+//     console.log(tenantId);
+//   });
+//   return deleteBtn;
+// }
